dc91b448311e341393e61a2897711d996e26b7b2
Jacob Lopez <jacobtl2342@gmail.com>
2019-10-11 23:42:22 -0500

    project setup
    initial project skeleton w/node, express, and bootstrap

---
+++ server.js
@@ -0,0 +1,10 @@
+//run with nodemon server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get("/",function(request, response){
+    response.sendFile(__dirname + "/index.html");
+
+});
+app.listen(port, () => console.log('Server is listening on port ${port}'));
+
