54b25f6dabb03fa32091ebdc766cec0afbb7ed46
Jacob Lopez <jacobtl2342@gmail.com>
2019-10-12 03:11:25 -0500

    Merge branch 'master' of https://github.com/PayPalHack-Chicago/Team-18

---
+++ script.js
@@ -0,0 +1,29 @@
+// Your web app's Firebase configuration
+exports.fire = function() {
+        // Firebase App (the core Firebase SDK) is always required and
+        // must be listed before other Firebase SDKs
+        var firebase = require("firebase/app");
+
+        // Add the Firebase products that you want to use
+        require("firebase/auth");
+        require("firebase/firestore");
+        //firebase.analytics();
+
+            const externKey = require('./firebase');
+
+            let firebaseConfig = externKey.key(); 
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+            
+            let db = firebase.firestore();
+        
+            db.collection('tutors').get()
+            .then((snapshot) => {
+                snapshot.forEach((doc) => {
+                console.log(doc.id, '=>', doc.data());
+                });
+            })
+            .catch((err) => {
+                console.log('Error getting documents', err);
+            });
+        }
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 //run with nodemon server.js
 const express = require('express');
+
+const script = require('./script');
+script.fire();
+
 const app = express();
 const port = 3000;
 app.use(express.static(__dirname + '/styles'));
