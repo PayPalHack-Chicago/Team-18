e1c88d7278b31a7575755b5d941f13a5512a2c6a
greyes1 <thatgreyes@gmail.com>
2019-10-12 02:56:53 -0500

    condensed firebase functionality to script file

--- server.js
+++ server.js
@@ -1,32 +1,8 @@
 //run with nodemon server.js
 const express = require('express');
 
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-//require("firebase/auth");
-require("firebase/firestore");
-//firebase.analytics();
-
-const externKey = require('./firebase');
-
-let firebaseConfig = externKey.key(); 
-     // Initialize Firebase
-     firebase.initializeApp(firebaseConfig);
-    
-     let db = firebase.firestore();
- 
-     db.collection('tutors').get()
-       .then((snapshot) => {
-         snapshot.forEach((doc) => {
-           console.log(doc.id, '=>', doc.data());
-         });
-       })
-       .catch((err) => {
-         console.log('Error getting documents', err);
-       });
+const script = require('./script');
+script.fire();
 
 const app = express();
 const port = 3000;
