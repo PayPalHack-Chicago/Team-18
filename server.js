411a14647d038992e6e0a5445fa7b6bb3c8cea41
Jacob Lopez <jacobtl2342@gmail.com>
2019-10-12 03:06:51 -0500

    css updates

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.static(__dirname + '/styles'));
+
 app.get("/",function(request, response){
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/pages/index.html");
 
 });
 app.listen(port, () => console.log('Server is listening on port ${port}'));
